adc30a412d95a39ba70ac8f1d5aad46d55cf09d8
mstingl <maximilian.stingl@inrae.fr>
2025-12-05 16:44:09 +0100

    update version number

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='ocmmed',
-    version='1.1.4',
+    version='1.2.0',
     packages=find_packages('.'),
     install_requires=requirements,
     include_package_data=True,
