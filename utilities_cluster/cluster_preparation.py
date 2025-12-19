ea0b454f95ea39ca841c9391c6b2e69c3de0e157
mstingl <maximilian.stingl@inrae.fr>
2025-12-19 15:15:49 +0100

    compute blocked and essential reactions in preparation

--- utilities_cluster/cluster_preparation.py
+++ utilities_cluster/cluster_preparation.py
@@ -2,6 +2,7 @@ import dexom_python
 import ruamel.yaml as yaml
 import random
 import os
+from cobra.flux_analysis import find_blocked_reactions, find_essential_reactions
 
 yaml_reader = yaml.YAML(typ='safe')
 with open('parameters.yaml', 'r') as file:
@@ -26,6 +27,12 @@ if __name__ == '__main__':
     if not params['rxn_enum_params']['reaction_list']:
         model = dexom_python.read_model(params['modelpath'])
         reactions = [r.id for r in model.reactions]
+
+        blocked_rxns = find_blocked_reactions(model)
+        essential_rxns = find_essential_reactions(model)
+
+        reactions = list(set(reactions) - set(blocked_rxns) - set(essential_rxns))
+
         random.shuffle(reactions)
         with open(outpath + 'reactions_shuffled.txt', 'w+') as file:
             file.write('\n'.join(reactions))
